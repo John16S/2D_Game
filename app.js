56baf784c6b26c86ef154266d98fb9db73f14471
Amin <aminjohn16sss@gmail.com>
2023-01-20 18:55:08 +0300

    Animation

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+var posX = 20;
+var posY = 20;
+var speedX = 5;
+var speedY = 5;
 
 function draw() {
-    context.beginPath(); //
-    context.arc(50, 50, 20, 0, Math.PI * 2, false); //false - против часовой стрелки
-    context.fillStyle = "rgb(8, 80, 139)";
-    context.fill();
-    context.closePath();
+  posX += speedX;
+  posY += speedY;
+  context.clearRect(0, 0, canvas.width, canvas.height); //очищение канваса
+
+  context.beginPath(); //
+  context.arc(posX, posY, 20, 0, Math.PI * 2, false); //false - против часовой стрелки
+  context.fillStyle = "rgb(8, 80, 139)";
+  context.fill();
+  context.closePath();
 }
 
-draw();
+setInterval(draw, 1000/30); //30 fps
